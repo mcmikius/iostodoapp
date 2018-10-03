23d4d0567089ba8582e6be7cc874ad4414779d78
Michail Bondarenko <mcmikius15@gmail.com>
2018-10-03 17:31:32 +0300

    fuck2

---
+++ iOSToDoApp/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  iOSToDoApp
+//
+//  Created by Michail Bondarenko on 10/3/18.
+//  Copyright © 2018 Michail Bondarenko. All rights reserved.
+//
+
+import Foundation
---
+++ iOSToDoApp/ItemsViewControllerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ItemsViewControllerViewController.swift
+//  iOSToDoApp
+//
+//  Created by Michail Bondarenko on 10/3/18.
+//  Copyright © 2018 Michail Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
